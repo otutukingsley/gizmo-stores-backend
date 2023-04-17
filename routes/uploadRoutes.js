aaaca575ff0b42f9b7b75b3796dea2e65345c69f
Otutu Chinedu Kingsley <chineduotutu3@gmail.com>
2023-04-17 16:10:45 +0100

    adjusted cloudinary upload functions testing

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,21 +1,21 @@
 import express from "express";
 import Product from "../models/productsModel.js";
-import cloudinary from "../utils/cloudinary.js"
+import cloudinary from "../utils/cloudinary.js";
 const router = express.Router();
 
 router.post("/:id", async (req, res) => {
-  const { imgUrl } = req.body;
+  const { src } = req.body;
   const product = await Product.findByIdAndUpdate(req.params.id);
-  console.log(req.body)
 
-  if (product && imgUrl) {
-    const imgSrc = await cloudinary.uploader.upload(`${imgUrl}`, { public_id: "gizmo_preset" })
-    res.json(imgSrc);
+  if (product && src) {
+    const res = await cloudinary.uploader.upload(src, {
+      public_id: "gizmo_preset",
+    });
+    res.json(res);
   } else {
     res.status(404);
     throw new Error("Invalid image format");
   }
-
 });
 
 export default router;
