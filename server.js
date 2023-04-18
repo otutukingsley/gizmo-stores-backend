5638e474170d4ed50dd8f563b2685222ff7d01ff
Otutu Chinedu Kingsley <chineduotutu3@gmail.com>
2023-04-18 17:17:48 +0100

    updated image upload route

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
-import path, { dirname } from "path";
 import connectDB from "./config/db.js";
 import productRoute from "./routes/productRoute.js";
 import userRoute from "./routes/userRoute.js";
@@ -14,7 +13,11 @@ dotenv.config();
 connectDB();
 const app = express();
 
-app.use(express.json());
+app.use(
+  express.json({
+    limit: "100mb",
+  })
+);
 
 app.use(
   cors({
@@ -33,16 +36,12 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use("/api/upload", uploadRoutes);
 app.use("/api/products", productRoute);
 app.use("/api/users", userRoute);
 app.use("/api/orders", orderRoute);
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
-
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use(notFound);
 app.use(errorHandler);
